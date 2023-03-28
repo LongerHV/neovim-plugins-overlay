f9e0d45433a00c7364f5c5bc1a4b928dbdbe5490
Michał Mieszczak <michal@mieszczak.com.pl>
2023-03-28 22:32:54 +0200

    Revert "Add nvim-treesitter"
    This reverts commit b2c278df145bd3a38d4cab4470b48a6ed90a7079.

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 {
   description = "Neovim plugin overlay";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nvim-cmp = {
       url = "github:hrsh7th/nvim-cmp";
       flake = false;
@@ -72,10 +71,6 @@
     };
     nvim-web-devicons = {
       url = "github:kyazdani42/nvim-web-devicons";
-      flake = false;
-    };
-    nvim-treesitter = {
-      url = "github:nvim-treesitter/nvim-treesitter";
       flake = false;
     };
     nvim-treesitter-textobjects = {
@@ -192,7 +187,6 @@
     , yaml-companion
     , nvim-tree
     , nvim-web-devicons
-    , nvim-treesitter
     , nvim-treesitter-textobjects
     , nvim-ts-rainbow
     , nvim-dap
@@ -309,10 +303,6 @@
             version = src.lastModifiedDate;
             src = nvim-web-devicons;
           };
-          nvim-treesitter = prev.pkgs.vimPlugins.nvim-treesitter.overrideAttrs (_: rec {
-            version = src.lastModifiedDate;
-            src = nvim-treesitter;
-          });
           nvim-treesitter-textobjects = prev.pkgs.vimUtils.buildVimPluginFrom2Nix rec {
             pname = "nvim-treesitter-textobjects";
             version = src.lastModifiedDate;
