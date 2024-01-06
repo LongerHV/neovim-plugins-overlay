ed48b37eae5f3e51a7ed12fb2c7837639107ca80
Michał Mieszczak <michal@mieszczak.com.pl>
2024-01-06 23:42:58 +0100

    Refactor flake

--- flake.nix
+++ flake.nix
@@ -50,98 +50,39 @@
       flake = false;
     };
   };
-  outputs =
-    { self
-    , nixpkgs
-    , nvim-lspconfig
-    , plenary
-    , oceanic-next
-    , telescope
-    , telescope-file-browser
-    , mini
-    , indent-blankline
-    , devicons
-    , gitsigns
-    , dressing
-    , gen
-    , fidget
-    }:
+  outputs = { self, nixpkgs, ... }@inputs:
     let
       forAllSystems = nixpkgs.lib.genAttrs [ "aarch64-linux" "x86_64-linux" ];
+      overlay = final: prev:
+        let
+          mkPlugin = name: value:
+            prev.pkgs.vimUtils.buildVimPlugin {
+              pname = name;
+              version = value.lastModifiedDate;
+              src = value;
+            };
+          plugins = prev.lib.filterAttrs (name: _: name != "self" && name != "nixpkgs") inputs;
+        in
+        {
+          nvimPlugins = builtins.mapAttrs mkPlugin plugins;
+        };
     in
-
     {
       formatter = forAllSystems (system: nixpkgs.legacyPackages."${system}".nixpkgs-fmt);
-      overlays.default = final: prev: {
-        nvimPlugins = {
-          nvim-lspconfig = prev.pkgs.vimUtils.buildVimPlugin rec {
-            pname = "nvim-lspconifg";
-            version = src.lastModifiedDate;
-            src = nvim-lspconfig;
-          };
-          plenary = prev.pkgs.vimUtils.buildVimPlugin rec {
-            pname = "plenary";
-            version = src.lastModifiedDate;
-            src = plenary;
-          };
-          oceanic-next = prev.pkgs.vimUtils.buildVimPlugin rec {
-            pname = "oceanic-next";
-            version = src.lastModifiedDate;
-            src = oceanic-next;
-          };
-          telescope = prev.pkgs.vimUtils.buildVimPlugin rec {
-            pname = "telescope";
-            version = src.lastModifiedDate;
-            src = telescope;
-          };
-          telescope-file-browser = prev.pkgs.vimUtils.buildVimPlugin rec {
-            pname = "telescope-file-browser";
-            version = src.lastModifiedDate;
-            src = telescope-file-browser;
-          };
-          mini = prev.pkgs.vimUtils.buildVimPlugin rec {
-            pname = "mini";
-            version = src.lastModifiedDate;
-            src = mini;
-          };
-          indent-blankline = prev.pkgs.vimUtils.buildVimPlugin rec {
-            pname = "indent-blankline";
-            version = src.lastModifiedDate;
-            src = indent-blankline;
-          };
-          devicons = prev.pkgs.vimUtils.buildVimPlugin rec {
-            pname = "devicons";
-            version = src.lastModifiedDate;
-            src = devicons;
-          };
-          gitsigns = prev.pkgs.vimUtils.buildVimPlugin rec {
-            pname = "gitsigns";
-            version = src.lastModifiedDate;
-            src = gitsigns;
-          };
-          dressing = prev.pkgs.vimUtils.buildVimPlugin rec {
-            pname = "dressing";
-            version = src.lastModifiedDate;
-            src = dressing;
-          };
-          gen = prev.pkgs.vimUtils.buildVimPlugin rec {
-            pname = "gen";
-            version = src.lastModifiedDate;
-            src = gen;
-          };
-          fidget = prev.pkgs.vimUtils.buildVimPlugin rec {
-            pname = "fidget";
-            version = src.lastModifiedDate;
-            src = fidget;
-          };
-        };
-      };
+      legacyPackages = forAllSystems (system:
+        import inputs.nixpkgs {
+          inherit system;
+          overlays = [ overlay ];
+          config.allowUnfree = true;
+        }
+      );
+      overlays.default = overlay;
       nixosConfigurations.test = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ({ pkgs, ... }: {
             boot.isContainer = true;
-            nixpkgs.overlays = [ self.overlays.default ];
+            nixpkgs.overlays = [ overlay ];
             system.stateVersion = "22.11";
             programs.neovim = {
               enable = true;
